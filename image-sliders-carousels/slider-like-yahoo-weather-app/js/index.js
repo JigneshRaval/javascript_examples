dbf49cc2b54fa3a2f2138c37c4e2ae2de1176fa2
Jignesh Raval <jignesh.rawal@alight.com>
2018-02-28 21:34:09 +0530

    Added RXJs and Slider example, Added tether.js for Bootstrap 4

--- image-sliders-carousels/slider-like-yahoo-weather-app/js/index.js
+++ image-sliders-carousels/slider-like-yahoo-weather-app/js/index.js
@@ -1,60 +1,86 @@
 var slider = {
-  
+
   // Not sure if keeping element collections like this
   // together is useful or not.
   el: {
     slider: $("#slider"),
     allSlides: $(".slide"),
     sliderNav: $(".slider-nav"),
-    allNavButtons: $(".slider-nav > a")
+    allNavButtons: $(".slider-nav > a"),
+    sliderWrapper: $(".slider-wrap")
   },
-  
+
   timing: 800,
+  isPlaying: true,
+  autoPlayInterval: 1500,
+  slideChangeInterval: undefined,
   slideWidth: 300, // could measure this
- 
+
   // In this simple example, might just move the
   // binding here to the init function
-  init: function() {
+  init: function () {
     this.bindUIEvents();
   },
-  
-  bindUIEvents: function() {
+
+  bindUIEvents: function () {
     // You can either manually scroll...
-    this.el.slider.on("scroll", function(event) {
+    this.el.slider.on("scroll", function (event) {
       slider.moveSlidePosition(event);
     });
     // ... or click a thing
-    this.el.sliderNav.on("click", "a", function(event) {
+    this.el.sliderNav.on("click", "a", function (event) {
       slider.handleNavClick(event, this);
+    });
+
+    /* let playbtn = document.querySelector('#play');
+    playbtn.addEventListener('click', () => {
+      slider.playSlideShow();
+    }) */
+
+    this.el.sliderNav.on("click", "#play", (event) => {
+      setInterval(() => {
+        //slider.moveSlidePosition(event);
+        slider.handleNavClick(event, this);
+      }, 500);
+      //slider.handleNavClick(event, this);
     });
     // What would be cool is if it had touch
     // events where you could swipe but it
     // also kinda snapped into place.
   },
-  
-  moveSlidePosition: function(event) {
+
+  // Pause slideshow
+  playSlideShow: function(event) {
+    this.isPlaying = true;
+    this.slideChangeInterval = setInterval(() => {
+      //slider.moveSlidePosition(event);
+      slider.handleNavClick(event, this);
+    }, this.autoPlayInterval);
+  },
+
+  moveSlidePosition: function (event) {
     // Magic Numbers =(
     this.el.allSlides.css({
-      "background-position": $(event.target).scrollLeft()/6-100+ "px 0"
-    });  
+      "background-position": $(event.target).scrollLeft() / 6 - 100 + "px 0"
+    });
   },
-  
-  handleNavClick: function(event, el) {
+
+  handleNavClick: function (event, el) {
     event.preventDefault();
     var position = $(el).attr("href").split("-").pop();
-    
+
     this.el.slider.animate({
       scrollLeft: position * this.slideWidth
     }, this.timing);
-    
+
     this.changeActiveNav(el);
   },
-  
-  changeActiveNav: function(el) {
+
+  changeActiveNav: function (el) {
     this.el.allNavButtons.removeClass("active");
     $(el).addClass("active");
   }
-  
+
 };
 
 slider.init();
