4ed818567eace539809f925d95d25da8cca06d4e
Jignesh Raval <jigneshraval2009@gmail.com>
2019-05-23 21:36:42 +0530

    Minor code changes.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ http.createServer(function (request, response) {
 
     var filePath = '.' + request.url;
     if (filePath == './')
-        filePath = './systemjs-typescript.html';
+        filePath = './code-editor-simple-js.html';
 
 var extname = path.extname(filePath);
     var contentType = 'text/html';
